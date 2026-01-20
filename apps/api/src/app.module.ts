67f49dbb6d1445a592b3716b807d016782a87c27
Алексей Степанов <aleksejstepanov@stepanoff.dev-mac>
2026-01-20 12:15:33 +0300

    initial commit

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: Number(config.get<string>('DB_PORT')),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+
+        synchronize: false,
+        logging: config.get<string>('NODE_ENV') === 'development',
+
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
+})
+export class AppModule {}
